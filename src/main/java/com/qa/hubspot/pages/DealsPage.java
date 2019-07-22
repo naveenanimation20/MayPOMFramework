2eb455dc6570c4b71a82ea4a0e2b216dd8eb0a9f
Naveen Khunteta <NaveenKhunteta@Naveen-Mac.local>
2019-07-22 08:48:02 +0530

    created deals page class

---
+++ src/main/java/com/qa/hubspot/pages/DealsPage.java
@@ -0,0 +1,11 @@
+package com.qa.hubspot.pages;
+
+import com.qa.hubspot.base.BasePage;
+
+public class DealsPage extends BasePage{
+	
+	public void getDeals(){
+		System.out.println("get deals method");
+	}
+
+}
