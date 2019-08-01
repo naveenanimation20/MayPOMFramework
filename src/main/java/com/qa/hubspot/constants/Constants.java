4c969ec8755e8fcf680625b467b9b32369d7b7ce
Shanmugam Velayudham <53459599+shanmugamvelayudham@users.noreply.github.com>
2019-08-01 08:11:07 -0400

    update the login constant

--- src/main/java/com/qa/hubspot/constants/Constants.java
+++ src/main/java/com/qa/hubspot/constants/Constants.java
@@ -2,8 +2,8 @@ package com.qa.hubspot.constants;
 
 public class Constants {
 
-	public static final String LOGIN_PAGE_TITLE = "HubSpot Login123";
-	public static final String HOME_PAGE_TITLE = "Reports dashboard123";
+	public static final String LOGIN_PAGE_TITLE = "HubSpot Login";
+	public static final String HOME_PAGE_TITLE = "Reports dashboard";
 	public static final String HOME_PAGE_HEADER = "Sales Dashboard";
 
 }
